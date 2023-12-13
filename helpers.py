c41d1852ad192e2ed035d7b9fc0ac2a9f6428ec1
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-12-14 00:43:55 +1100

    🏗️ restructured and fixed code - neural networks are buzzing!

--- helpers.py
+++ helpers.py
@@ -1,23 +1,31 @@
 import pandas as pd
 import numpy as np
 
-def load_mnist_train():
-    df = pd.read_csv("data/mnist_train.csv")
-    labels = df.pop("label")
-    return labels.to_numpy(), df.to_numpy().astype(np.float32)
+def load_mnist():
+    train_X = pd.read_csv("data/mnist_train.csv")
+    train_y = train_X.pop("label")
 
+    val_X = pd.read_csv("data/mnist_test.csv")
+    val_y = val_X.pop("label")
 
+    return (
+        train_X.to_numpy().astype(np.float32), 
+        train_y.to_numpy(),
+        val_X.to_numpy().astype(np.float32),
+        val_y.to_numpy(),
+    )
 
+def one_hot(y):
+    return np.eye(len(np.unique(y)))[y]
 
-if __name__ == "__main__":
+def softmax(x: np.ndarray):
+    x = x - np.max(x, axis=0) # this is to avoid high values (it doesn't change the outputs)
+    e = np.exp(x)
+    s = e / np.sum(e, axis=0)
+    return s
 
-    # x = np.array([5, 7, 1])
-
-    # s = softmax(x)
-    # print(s)
-
-    # s_prime = softmax_prime(x)
-    # print(s_prime)
-
-    x = [1, 2, 3, 4, 5]
-    print(x[::-1])
+def cross_entropy(y_pred: np.ndarray, y_true: np.ndarray):
+    m = y_pred.shape[-1]
+    log_probs =  (-1) * np.sum( y_true * np.log(y_pred+1e-7) )
+    loss = log_probs / m
+    return float(loss)
--- layers.py
+++ layers.py
@@ -1,34 +1,62 @@
 import numpy as np
-from helpers import load_mnist_train
-from losses import cross_entropy, cross_entropy_prime
-
+from helpers import load_mnist, one_hot, softmax, cross_entropy
 
 class Layer:
+    """
+    Abstract class for a neura network layer.
+    """
     def __init__(self, name):
+        """
+        name : str
+            Name of the layer, defaults to "Dense".
+        """
         self.name = name
 
     def forward(self, input: np.ndarray):
+        """
+        input : np.ndarray
+            Input to the layer. Shape = (input_dim, batch)
+        """
         self.input = input
 
     def backward(self, output_gradient: np.ndarray, learning_rate: float):
+        """
+        output_gradient : np.ndarray
+            Derivative of the loss with respect to this layer's outputs.
+        
+        learning_rate : float
+            Learning rate.
+        """
         pass
 
 
 class Dense(Layer):
     """
-    Fully Connected Layer of Neurons
+    Fully Connected Layer of Neurons.
     """
-    def __init__(self, input_size, output_size, name="Dense"):
-        self.weights = np.random.uniform(-1, 1, (output_size, input_size))
+    def __init__(self, input_size: int, output_size: int, name:str="Dense"):
+        """
+        input_size : int
+            Number of input dimensions to the layer.
+        
+        output_size: int
+            Number of hidden neurons.
+
+        name : int
+            Name of the layer, defaults to "Dense".
+        """
+        self.weights = np.random.randn(output_size, input_size) * np.sqrt(2/input_size)
         self.bias = np.zeros((output_size, 1))
         super().__init__(name)
+
     def forward(self, input: np.ndarray):
         self.input = input
         return np.dot(self.weights, input) + self.bias
     
     def backward(self, output_gradient: np.ndarray, learning_rate: float):
-        grad_weights = output_gradient @ self.input.T
-        grad_bias = output_gradient
+        m = self.input.shape[-1] # get batch size from input
+        grad_weights = (1/m) * output_gradient @ self.input.T
+        grad_bias = (1/m) * np.sum(output_gradient, axis=-1, keepdims=True)
         self.weights -= learning_rate * grad_weights
         self.bias -= learning_rate * grad_bias
         return self.weights.T @ output_gradient # return the derivative of the loss with respect to inputs
@@ -36,7 +64,7 @@ class Dense(Layer):
 
 class Activation(Layer):
     """
-    Activation function
+    Activation function.
     """
     def __init__(self, activation, activation_prime, name):
         self.activation = activation
@@ -62,98 +90,91 @@ class ReLU(Activation):
         return output_gradient  * self.activation_prime(self.input)
 
 
-class Softmax(Activation):
-    """
-    Softmax activation.
-    Converts outputs to probability distribution.
-    """
-    def __init__(self, name):
-        def softmax(x: np.ndarray):
-            x = x - np.max(x) # this is to avoid high values (it doesn't change the outputs)
-            e = np.exp(x)
-            s = e / np.sum(e)
-            s = s + 1e-9 # make sure there are no zeros
-            return s
-
-        def softmax_prime(x: np.ndarray):
-            """
-            Calculate Jacobian matrix for Softmax (all partial derivatives with respect to all inputs)
-            """
-            s = softmax(x).flatten()
-            n = len(x)
-            jac = np.zeros((n, n))
-            for i in range(n):
-                for j in range(n):
-                    if i == j:
-                        jac[i][j] = s[i] * (1 - s[i])
-                    else:
-                        jac[i][j] = (-1) * s[i] * s[j]
-            return jac
-
-        super().__init__(softmax, softmax_prime, name)
-
-    def backward(self, output_gradient: np.ndarray, learning_rate: float):
-        """
-        The derivative of the softmax function is a Jacobian matrix.
-        To calculate the derivative of the loss with respect to the inputs of the softmax, matrix multiply the Jacobian (derivative of the Softmax) with the derivative of the Cross entropy loss with respect to the outputs of the network.
-        This result should be the same as (y_pred - y_true)
-        """
-        return self.activation_prime(self.input).T @ output_gradient
-
-
-def softmax(x: np.ndarray):
-    # x = x - np.max(x) # this is to avoid high values (it doesn't change the outputs)
-    e = np.exp(x)
-    s = e / np.sum(e)
-    return s
-
 if __name__ == "__main__":
+
+    # DEFINE LAYERS
     layers = [
         Dense(784, 16, "Input Dense"), # input = (784, 1) | output = (16, 1)
         ReLU("Relu"), # input = (16, 1) | output = (16, 1)
         Dense(16, 10, "Hidden Dense"), # input = (16, 1) | output = (10, 1)
     ]
 
-    # load data
-    labels, mnist = load_mnist_train()
-    n = len(mnist)
+    # LOAD DATA
+    train_X, train_y, val_X, val_y = load_mnist()
 
-    # transform data
-    mnist = mnist.T / 255
-    labels = np.eye(len(np.unique(labels)))[labels].T
+    # PREPARE DATA
+    train_X = train_X.T / 255 # (784, trainN)
+    train_y = one_hot(train_y).T # (10, trainN)
+    val_X = val_X.T / 255 # (784, valN)
+    val_y = one_hot(val_y).T # (10, valN)
 
-    epochs = 10
+    print(f"Training X:  {train_X.shape}")
+    print(f"Training Y:  {train_y.shape}")
+    print(f"Val X:  {val_X.shape}")
+    print(f"Val Y:  {val_y.shape}")
 
-    for e in range(epochs):
+    # PARAMETERS
+    trainN = train_X.shape[-1]
+    valN = val_X.shape[-1]
+    BATCH_SIZE = 100
+    EPOCHS = 100
 
-        # print(f"Epoch {e}")
+    for e in range(1, EPOCHS+1):
 
-        total_loss = 0
+        # TRAINING LOOP (FORWARD PROP, BACK PROP)
 
-        for i in range(n):
-
-            # get sample
-            X = mnist[:, i:i+1]
-            y = labels[:, i:i+1]
-
+        print(f"Epoch {e}... ", end="")
+        train_loss = 0
+        train_acc = 0
+        for i in range(0, trainN, BATCH_SIZE):
+            # get batch
+            X = train_X[:, i:i+BATCH_SIZE] # (784, BATCH)
+            y = train_y[:, i:i+BATCH_SIZE] # (10, BATCH)
             # forward prop
             output = X
             for layer in layers:
                 output = layer.forward(output)
-            
             # softmax
             y_pred = softmax(output)
-            print(np.sum(y_pred))
-
             # calculate loss
             loss = cross_entropy(y_pred, y)
-            # print(loss)
-
+            train_loss += loss
+            # calculate accuracy
+            acc = np.sum(np.equal(np.argmax(y_pred, axis=0), np.argmax(y, axis=0)))
+            acc /= BATCH_SIZE
+            train_acc += acc
             # back prop
             grad = y_pred - y # derivative of the loss with respect to outputs of last dense layer
             learning_rate = 0.01
-
             for layer in layers[::-1]:
                 grad = layer.backward(grad, learning_rate)
+        # divide metrics by number of batches
+        train_loss = train_loss / ( trainN / BATCH_SIZE ) 
+        train_acc = train_acc / ( trainN / BATCH_SIZE ) 
+        print(f"Training Loss: {round(train_loss, 5)} | Training Accuracy: {round(train_acc, 3)} | ", end="")
 
-        # print(f"Epoch Avg loss: {total_loss/n}")
+        # VALIDATION LOOP (FORWARD PROP, NO BACK PROP)
+
+        val_loss = 0
+        val_acc = 0
+        for i in range(0, valN, BATCH_SIZE):
+            # get batch
+            X = val_X[:, i:i+BATCH_SIZE] # (784, BATCH)
+            y = val_y[:, i:i+BATCH_SIZE] # (10, BATCH)
+            # forward prop
+            output = X
+            for layer in layers:
+                output = layer.forward(output)
+            # softmax
+            y_pred = softmax(output)
+            # calculate loss
+            loss = cross_entropy(y_pred, y)
+            val_loss += loss
+            # calculate accuracy
+            acc = np.sum(np.equal(np.argmax(y_pred, axis=0), np.argmax(y, axis=0)))
+            acc /= BATCH_SIZE
+            val_acc += acc
+        # divide metrics by number of batches
+        val_loss = val_loss / ( valN / BATCH_SIZE )
+        val_acc = val_acc / ( valN / BATCH_SIZE ) 
+        print(f"Validation Loss: {round(val_loss, 5)} | Validation Accuracy: {round(val_acc, 3)}")
